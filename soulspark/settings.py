1b8ddf9bcac14e8ddee2a1f62dc9c0244b68dd31
ElisabethKAndersson <>
2023-11-01 12:09:37 +0000

    Project set up, env.py error

--- soulspark/settings.py
+++ soulspark/settings.py
@@ -15,11 +15,15 @@ import os
 import dj_database_url
 
 if os.path.isfile("env.py"):
-    import env
+    import env.py
+
+print("------- DATABASE URL -------")
+print(os.environ.get("DATABASE_URL"))
+print("----------------------------")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -30,7 +34,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-elisabethkandersson-port-nud0uk3l20.us2.codeanyapp.com']
+ALLOWED_HOSTS = ['soulspark.herokuapp.com', 'localhost',
+                 '8000-elisabethkandersson-port-nud0uk3l20.us2.codeanyapp.com']
 
 
 # Application definition
@@ -41,7 +46,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary'
     'presentation',
 ]
 
@@ -60,7 +67,7 @@ ROOT_URLCONF = 'soulspark.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,6 +135,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
