ec92930312c3f9edfbcc2122a323d1061f1f438f
ElisabethKAndersson <>
2023-11-03 20:35:46 +0000

    Add templates folder with index.html functioning

--- presentation/views.py
+++ presentation/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, reverse
+from django.views import generic
+
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, "presentation/index.html")
