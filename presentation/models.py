513fdf7712d6f3edefb1c22c7e082691989561f7
ElisabethKAndersson <>
2023-11-13 20:49:27 +0000

    add authentication

--- presentation/models.py
+++ presentation/models.py
@@ -1,14 +1,63 @@
-from django.db import models
-from django.contrib.auth.models import User
-from django.contrib.auth import get_user_model
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views import generic
+from .models import Item
+from .forms import ItemForm
+# from .forms import LeaveReview, Post
+# Create your views here.
 
 
-class Item(models.Model):
-    context = models.TextField(max_length=200, default="Add your review")
-    name = models.CharField(max_length=200, default="Add your name")
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="reviews", default=1
-    )
+# Index page
+def index(request):
+    return render(request, "presentation/index.html")
 
-    def __str__(self):
-        return self.name
+
+# Prices page
+def prices(request):
+    return render(request, "presentation/prices.html")
+
+
+# Contact page
+def contact(request):
+    return render(request, "presentation/contact.html")
+
+
+# Review page
+def reviews(request):
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, 'presentation/reviews.html', context)
+
+
+def leave_review(request):
+    if request.method == 'POST':
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('reviews')
+    form = ItemForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'presentation/leave_review.html', context)
+
+
+def edit_review(request):
+    item = get_object_or_404(Item, id=item_id)
+    if request.method == 'POST':
+        form = ItemForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('reviews')
+    form = ItemForm(instance=item)
+    context = {
+        'form': form
+    }
+    return render(request, 'presentation/reviews.html', context)
+
+
+def delete_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    item.delete()
+    return redirect('presentation/reviews.html')
