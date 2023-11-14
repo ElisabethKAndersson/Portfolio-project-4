ecfac59f68663b7ac876b14bae8fe2ee5d2cfdfa
ElisabethKAndersson <>
2023-11-14 10:15:49 +0000

    debug false

--- soulspark/settings.py
+++ soulspark/settings.py
@@ -33,7 +33,9 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['soulspark.herokuapp.com', 'localhost',
                  '8000-elisabethkandersson-port-0nyl954cem.us2.codeanyapp.com',]
@@ -103,12 +105,12 @@ WSGI_APPLICATION = 'soulspark.wsgi.application'
 # Database
 # 'https://docs.djangoproject.com/en/3.2/ref/settings/#databases'
 
-#DATABASES = {
+# DATABASES = {
 #    'default': {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': BASE_DIR / 'db.sqlite3',
 #    }
-#}
+# }
 
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
