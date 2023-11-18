057d3fad975e7ffef02a9fcbec5a823e846d0a78
ElisabethKAndersson <>
2023-11-18 19:06:32 +0000

    change debug to false

--- soulspark/settings.py
+++ soulspark/settings.py
@@ -33,7 +33,7 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
