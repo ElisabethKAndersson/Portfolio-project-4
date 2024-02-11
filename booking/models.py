c3408f59fb154ab14b58478b2c3a05a837920514
Elisabeth Andersson <flisanlisan@gmail.com>
2024-02-11 22:55:36 +0000

    Add model for booking

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import User
 
-# Create your models here.
+SERVICE_CHOICES = (
+    ("Soulcoaching 1h", "Soulcoaching 1h"),
+    ("Soulcoaching Expansion 1,5h", "Soulcoaching Expansion 1,5h"),
+    )
+
+TIME_CHOICES = (
+    ("09:00", "09:00"),
+    ("10:00", "10:00"),
+    ("11:00", "11:00"),
+    ("13:00", "13:00"),
+    ("14:00", "14:00"),
+    ("15:00", "15:00"),
+    ("16:00", "16:00"),
+)
+
+class Appointment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    service = models.CharField(max_length=50, choices=SERVICE_CHOICES, default="Soulcoaching")
+    day = models.DateField(default=datetime.now)
+    time = models.CharField(max_length=10, choices=TIME_CHOICES, default="09:00")
+    time_ordered = models.DateTimeField(default=datetime.now, blank=True)
+    def __str__(self):
+        return f"{self.user.username} | day: {self.day} | time: {self.time}"
