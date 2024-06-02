37540797443a28b671a7b98e4d5921faace7f187
Elisabeth Andersson <flisanlisan@gmail.com>
2024-06-02 15:54:21 +0000

    Change  order of installed apps in settings

--- soulspark/settings.py
+++ soulspark/settings.py
@@ -44,21 +44,23 @@ ALLOWED_HOSTS = ['soulspark-2b23afca50ab.herokuapp.com', 'localhost',
 # Application definition
 
 INSTALLED_APPS = [
-    'presentation.apps.PresentationConfig',
     'django.contrib.admin',
-    'django.contrib.sites',
     'django.contrib.auth',
-    'allauth',
-    'allauth.account',
-    'allauth.socialaccount',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     
     'django.contrib.staticfiles',
     'cloudinary_storage',
     'cloudinary',
     'crispy_forms',
+
+    'presentation.apps.PresentationConfig',
 ]
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
